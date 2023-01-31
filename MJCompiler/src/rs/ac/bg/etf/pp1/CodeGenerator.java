ba35f25c1ea3f1ff2375d4281bceb42db216dc73
Stefan Brankovic <79327006+stefcon@users.noreply.github.com>
2023-01-31 21:39:39 +0100

    Added semantic check for expressions, classes, print and read

--- MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ MJCompiler/src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -1,21 +1,7 @@
 package rs.ac.bg.etf.pp1;
 
-import rs.ac.bg.etf.pp1.CounterVisitor.FormParamCounter;
-import rs.ac.bg.etf.pp1.CounterVisitor.VarCounter;
-import rs.ac.bg.etf.pp1.ast.AddExpr;
-import rs.ac.bg.etf.pp1.ast.Assignment;
-import rs.ac.bg.etf.pp1.ast.Const;
-import rs.ac.bg.etf.pp1.ast.Designator;
-import rs.ac.bg.etf.pp1.ast.FormalParamDecl;
-import rs.ac.bg.etf.pp1.ast.FuncCall;
-import rs.ac.bg.etf.pp1.ast.MethodDecl;
-import rs.ac.bg.etf.pp1.ast.MethodTypeName;
-import rs.ac.bg.etf.pp1.ast.PrintStmt;
-import rs.ac.bg.etf.pp1.ast.ReturnExpr;
-import rs.ac.bg.etf.pp1.ast.ReturnNoExpr;
-import rs.ac.bg.etf.pp1.ast.SyntaxNode;
-import rs.ac.bg.etf.pp1.ast.VarDecl;
-import rs.ac.bg.etf.pp1.ast.VisitorAdaptor;
+import rs.ac.bg.etf.pp1.CounterVisitor.*;
+import rs.ac.bg.etf.pp1.ast.*;
 import rs.etf.pp1.mj.runtime.Code;
 import rs.etf.pp1.symboltable.concepts.Obj;
 
@@ -40,19 +26,24 @@ public class CodeGenerator extends VisitorAdaptor {
 		
 		// Collect arguments and local variables.
 		SyntaxNode methodNode = MethodTypeName.getParent();
-		VarCounter varCnt = new VarCounter();
+		VariableDeclarationCounter varCnt = new VariableDeclarationCounter();
 		methodNode.traverseTopDown(varCnt);
-		FormParamCounter fpCnt = new FormParamCounter();
+		FormalParamCounter fpCnt = new FormalParamCounter();
 		methodNode.traverseTopDown(fpCnt);
 		
 		// Generate the entry.
 		Code.put(Code.enter);
-		Code.put(fpCnt.getCount());
-		Code.put(varCnt.getCount() + fpCnt.getCount());
+		Code.put(fpCnt.getCounter());
+		Code.put(varCnt.getCounter() + fpCnt.getCounter());
 	}
 	
 	@Override
-	public void visit(VarDecl VarDecl) {
+	public void visit(VarNameSingle VarDecl) {
+		varCount++;
+	}
+	
+	@Override
+	public void visit(VarNameArray VarDecl) {
 		varCount++;
 	}
 
@@ -67,46 +58,46 @@ public class CodeGenerator extends VisitorAdaptor {
 		Code.put(Code.return_);
 	}
 	
-	@Override
-	public void visit(ReturnExpr ReturnExpr) {
-		Code.put(Code.exit);
-		Code.put(Code.return_);
-	}
+//	@Override
+//	public void visit(ReturnExpr ReturnExpr) {
+//		Code.put(Code.exit);
+//		Code.put(Code.return_);
+//	}
+//	
+//	@Override
+//	public void visit(ReturnNoExpr ReturnNoExpr) {
+//		Code.put(Code.exit);
+//		Code.put(Code.return_);
+//	}
+//	
+//	@Override
+//	public void visit(Assignment Assignment) {
+//		Code.store(Assignment.getDesignator().obj);
+//	}
+//	
+//	@Override
+//	public void visit(Const Const) {
+//		Code.load(new Obj(Obj.Con, "$", Const.struct, Const.getN1(), 0));
+//	}
+//	
+//	@Override
+//	public void visit(Designator Designator) {
+//		SyntaxNode parent = Designator.getParent();
+//		if (Assignment.class != parent.getClass() && FuncCall.class != parent.getClass()) {
+//			Code.load(Designator.obj);
+//		}
+//	}
 	
 	@Override
-	public void visit(ReturnNoExpr ReturnNoExpr) {
-		Code.put(Code.exit);
-		Code.put(Code.return_);
-	}
-	
-	@Override
-	public void visit(Assignment Assignment) {
-		Code.store(Assignment.getDesignator().obj);
-	}
-	
-	@Override
-	public void visit(Const Const) {
-		Code.load(new Obj(Obj.Con, "$", Const.struct, Const.getN1(), 0));
-	}
-	
-	@Override
-	public void visit(Designator Designator) {
-		SyntaxNode parent = Designator.getParent();
-		if (Assignment.class != parent.getClass() && FuncCall.class != parent.getClass()) {
-			Code.load(Designator.obj);
-		}
-	}
-	
-	@Override
-	public void visit(FuncCall FuncCall) {
-		Obj functionObj = FuncCall.getDesignator().obj;
+	public void visit(FunctionCall FuncCall) {
+		Obj functionObj = FuncCall.getFunctionName().getDesignator().obj;
 		int offset = functionObj.getAdr() - Code.pc; 
 		Code.put(Code.call);
 		Code.put2(offset);
 	}
 	
 	@Override
-	public void visit(PrintStmt PrintStmt) {
+	public void visit(PrintStatement PrintStmt) {
 		Code.put(Code.const_5);
 		Code.put(Code.print);
 	}
--- MJCompiler/test/rs/ac/bg/etf/pp1/MJParserTest.java
+++ MJCompiler/test/rs/ac/bg/etf/pp1/MJParserTest.java
@@ -9,6 +9,7 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 
 import java_cup.runtime.Symbol;
+import rs.ac.bg.etf.pp1.ast.Program;
 import rs.ac.bg.etf.pp1.ast.SyntaxNode;
 import rs.ac.bg.etf.pp1.util.Log4JUtils;
 import rs.etf.pp1.mj.runtime.Code;
@@ -23,12 +24,19 @@ public class MJParserTest {
 	
 	public static void main(String[] args) throws Exception {
 		Logger log = Logger.getLogger(MJParserTest.class);
-		if (args.length < 2) {
-			log.error("Not enough arguments supplied! Usage: MJParser <source-file> <obj-file> ");
+		File sourceCode;
+		if (args.length == 2) {
+			sourceCode = new File(args[0]);
+		}
+		else if (args.length == 0) {
+			sourceCode = new File("program.mj");
+		}
+		else {
+			log.error("Not enough arguments supplied! Usage: MJParser <source-file> <obj-file> or no arguments at all!");
 			return;
 		}
 		
-		File sourceCode = new File(args[0]);
+		
 		if (!sourceCode.exists()) {
 			log.error("Source file [" + sourceCode.getAbsolutePath() + "] not found!");
 			return;
@@ -40,20 +48,28 @@ public class MJParserTest {
 			Yylex lexer = new Yylex(br);
 			MJParser p = new MJParser(lexer);
 	        Symbol s = p.parse();  //pocetak parsiranja
-	        SyntaxNode prog = (SyntaxNode)(s.value);
+	        Program prog = (Program)(s.value);
 	        
 			Tab.init(); // Universe scope
-			SemanticPass semanticCheck = new SemanticPass();
+			SemanticAnalyzer semanticCheck = new SemanticAnalyzer();
 			prog.traverseBottomUp(semanticCheck);
 			
-	        log.info("Print calls = " + semanticCheck.printCallCount);
+//	        log.info("Print calls = " + semanticCheck.printCallCount);
 	        Tab.dump();
 	        
+	        log.info("===============================");
+	        log.info(prog.toString(""));
+	        log.info("===============================");
+	        
 	        if (!p.errorDetected && semanticCheck.passed()) {
-	        	File objFile = new File(args[1]);
-	        	log.info("Generating bytecode file: " + objFile.getAbsolutePath());
-	        	if (objFile.exists())
-	        		objFile.delete();
+	        	log.info("Parsiranje uspesno zavrseno (bez generisanja, za sad)!");
+	        	
+//	        	File objFile;
+//	        	if (args.length > 0) objFile = new File(args[1]);
+//	        	else objFile = new File("program.obj");
+//	        	log.info("Generating bytecode file: " + objFile.getAbsolutePath());
+//	        	if (objFile.exists())
+//	        		objFile.delete();
 	        	
 	        	// Code generation... TODO: Dok se ne proveri semantic pass, ostaviti zakomentarisano
 //	        	CodeGenerator codeGenerator = new CodeGenerator();
